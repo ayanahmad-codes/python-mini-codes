5d07d68dbde3b15c0f5210a31298ec0f203125cb
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-01 01:48:29 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,41 @@
+print("hello world")
+#jio mart details
+shop_name = "Jio mart"
+shop_owner = "Ambani"
+print("welcome to",shop_name,"and the owner is",shop_owner)
+
+
+name = "anjum"
+age = 17
+height = 5.5
+address = "salam colony ward number 12"
+price = 200
+print(name)
+print(age)
+print(address)
+print(height)
+print(price)
+
+a = 6
+b = 78
+c = a + b
+print(c)
+a = 900
+b = 1000
+sum = a - b 
+print(sum)
+
+#Type conversion
+a = 2
+b = 4.25
+sum = a + b # 2.0 + 4.25
+print(sum)
+
+#input value
+name = input("enter name: ")
+age = input("enter age: " )
+marks = input("enter marks: ")
+
+print("welcome", name)
+print("age =", age)
+print("marks =", marks)
