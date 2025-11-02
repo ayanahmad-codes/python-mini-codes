e87be0b979949fae27d8d63e4b19d3429ad98df0
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-02 21:35:47 +0530

    Add files via upload

---
+++ classtypecoversion.py
@@ -0,0 +1,20 @@
+# a = int(input("enter integer: "))
+# b = int(input("enter integer: "))
+# print(a + b)
+# print(a - b)
+# print(a % b)
+# print(a / b)
+
+a = 10
+b = "lion"
+c = 2.99
+print(type(a))
+print(type(b))
+print(type(c))
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(max(list))
+print(min(list))
+
+
+# convert a tuple into list, add 3 new elts, and conver it back into a tuple
