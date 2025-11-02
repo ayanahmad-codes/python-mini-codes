101a782243faed5a52b27c60d4c44082df6d7d4b
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-02 21:40:30 +0530

    Add files via upload

---
+++ classtest.py
@@ -0,0 +1,8 @@
+student_name = "Ayan ahmad"  
+_marks = 100
+# cinema hall trickets details
+print("The student name is",student_name,"and he got",_marks)
+movie_name = "Bahubali"
+ticket_price = 500
+print("I am going to watch",movie_name,"and the ticket price is",ticket_price)
+#traveller is going to buy tickets online
