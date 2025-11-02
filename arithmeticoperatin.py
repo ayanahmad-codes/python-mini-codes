d8b46f222a80b9e0ff878d0cc2c1413705d4dba2
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-02 21:35:19 +0530

    Add files via upload

---
+++ arithmeticoperatin.py
@@ -0,0 +1,41 @@
+a = 22
+b= 10
+
+# print(a + b)
+# print(a - b)
+# print(a % b)# remainder
+# print(a * b)#multiplication
+# print(a ** b)#a^b
+# print(a / b)#always give floating value divide
+
+#comparison operators
+a = 50
+b = 20
+
+# print(a == b) #False/equal sign
+# print(a != b) #False /not equal sign
+# print(a >= b) #True
+# print(a <= b)#True
+# print(a < b)#False
+# print(a > b)#True
+
+
+#assignment operators
+num = 10
+num += 5
+#num -= 5
+# num **= 5
+# num %= 5
+#print("num :", num)
+   
+
+#Logical operators 
+a = 50
+b = 30 
+# print(not False)
+# print(not (a > b)) #opposite answer  
+
+#print("AND operator:", 50 and 30)
+#print("OR operator:", val1 or val2)
+#print("OR operator:", (a == b) or (a > b))#True
+#print("AND operator:", (a == b) and (a > b))#False
