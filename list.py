22bc186b6d9b3df2cb26ce5ddf3618a8e387f7f9
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-02 21:38:38 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,47 @@
+# movie = []
+# movie1 = input(("enter movie: "))
+# movie2 = input(("enter movie: "))
+# movie3 = input(("enter movie: "))
+
+# movie.append(movie1) # list 
+# movie.append(movie2)
+# movie.append(movie3)
+# print(movie)
+
+# list = [1, 7, 5, 8,9]
+# list.append(2)
+# print(list)
+# movie = []
+# movie.append(input("enter 1st movie: ")) #short form
+# movie.append(input("enter 2nd movie: "))
+# movie.append(input("enter 3rd movie: "))
+# print(movie)
+
+
+# list1 =[1, 3, 3, 1]
+
+# copy_list1 = list1
+# copy_list1.reverse()
+
+# if(copy_list1 == list1):
+#     print("palidrome")
+# else:
+#     print("not palidrome")
+    
+# list1 =["ayan", "ankit", "ayan"]
+
+# copy_list1 = list1
+# copy_list1.reverse()
+
+# if(copy_list1 == list1):
+#     print("Future billionaire")
+# else:
+#     print("not palidrome")
+
+# grade = ("C", "D.", "A", "B", "A", "B", "A")
+# print(grade.count("A"))
+
+# grade = ["C", "D.", "A", "B", "A", "B", "A"]
+# grade.sort()
+# print(grade)
+
