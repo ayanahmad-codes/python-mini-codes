aae0843ebdde4619bbf28af7efb526caf5c1c675
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-02 21:39:30 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,69 @@
+# # first = int(input("enter first : "))
+# # second = int(input("enter second : "))
+# #print("sum =", first + second)
+
+# side = float(input("enter square side : "))
+# #print("area =", side * side)
+# #print("area =", side ** 2)
+
+# x = float(input("enter first : "))
+# y = float(input("enter second : "))
+# #print("average =", (x + y)/2)
+
+# a = int(input("enter first : "))
+# b = int(input("enter second : "))
+# #print(a >= b)
+
+
+# #marks = int(input("enter student marks :"))
+# # if(marks >= 90):
+# #     print("A grade")
+# # elif(marks >= 80 and marks < 90):
+# #     print("B grade")
+# # elif(marks >= 70 and marks > 80):
+# #     print("C grade")
+# # elif(marks >= 60 and marks > 50):
+# #     print("D grade")
+# # else:
+# #     print("grade of the student is, grade")
+
+
+
+# # num = int((input("enter a number :")))
+# # if(num % 2 == 0):
+# #     print("even number")
+# # else:
+# #     print("odd number")
+
+
+# # a = int(input("enter first number :"))
+# # b = int(input("enter second number :"))
+# # c = int(input("enter third number :"))
+
+# # if(a >= b and a >= c):
+# #     print("a is the greatest")
+# # elif(b >= a and b >= c):
+# #     print("b is the greatest")
+# # else:
+# #     print("c is the greatest")
+
+# y = int(input("enter number :"))
+# if(y % 7 == 0):
+#     print("multiple of 7")
+# else:
+#     print("not a multiple of 7")
+
+# x = int(input("enter first number :"))
+# y = int(input("enter second number :"))
+# a = int(input("enter third number :"))
+# b = int(input("enter fourth number :"))
+# if(x >= y and x >= a and x >= b):
+# 	print("x is the greatest")
+# elif(y >= x and y >= a and y >= b):
+# 	print("y is the greatest")
+# elif(a >= x and a >= y and a >=b):
+# 	print("a is the greatest")
+# else:
+# 	print("b is the greatest")
+
+
