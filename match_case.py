394cb3df4f5b7ebd5cf2d407b2e76968bb915cd8
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-05 23:48:45 +0530

    Add files via upload

---
+++ match_case.py
@@ -0,0 +1,21 @@
+x = 10
+match x:
+    case 0:
+        print("x is zero")
+    case 1:
+        print("x is one")
+    case 10:
+        print("x is ten")
+        pass
+
+# y = int(input("enter a value of y: "))
+# match y:
+#     case 1:
+#         print("case 1 ") 
+#     case 2:
+#         print("y is 2")
+#     case _ if y != 5:
+#         print("y is not 5")
+#     case _:
+#         print("y")
+
