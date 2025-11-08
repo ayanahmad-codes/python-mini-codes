8294ef6987e098c3a385df9f8f9e5931cb6e9f75
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-08 21:59:32 +0530

    Add files via upload

---
+++ def_funtion.py
@@ -0,0 +1,28 @@
+# def cal_prod(a , b=2):
+#     return a * b
+# cal_prod(2)
+
+
+# def converter(usd_val):
+#     x = int(input("Enter the USD value: "))
+#     inr_val = x * 82
+#     print("inr value is: ", inr_val)
+#     return inr_val
+# print(converter(7))
+
+# def num_check(num): 
+#     if num % 2 == 0:
+#         return "Even" # return sirf value ko return krta hai print nhi krta
+#     else:
+#         return "Odd"
+# x = int(input("Enter a number: "))
+# val = num_check(x) 
+# print(val) # yahan print function ke bahar likhna parega tabhi value print hogi
+
+# def num_check(num): 
+#     if num % 2 == 0:
+#         print("Even")# return sirf value ko return krta hai print nhi krta
+#     else:
+#         print("Odd")
+# x = int(input("Enter a number: "))
+# num_check(x)
