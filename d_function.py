c2d5cc8f9ba6935940bad48200753f8bd4dc7fde
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-08 21:58:57 +0530

    Add files via upload

--- d_function.py
+++ d_function.py
@@ -1,1 +1,82 @@
 #This is a sample Python function
+# def name():
+#     print("ayan")
+# name()
+
+# def greet():
+#     print("Hello Ayan!")
+# greet()
+
+# def num():
+#     for i in range(1,6):
+#         print(i)
+# num()
+
+# def evenOdd(a):
+#     if a % 2 ==0:
+#         print("even")
+#     else:
+#         print("odd")
+
+# evenOdd(11)
+
+# def myfun(x, y=20): # arguments are the values passes inside the parenthesis of the funtion
+
+    # print("x + y", "=", x+y)
+    # print("x + y =", x+y)
+    # print(x-y)
+    # print(x*y)
+    # print(x%y)
+    # print(x/y)
+    # print(x//y)
+    # print("x: ", x)
+    # print("y: ", y)
+
+# myfun(10)  # This is a function call // funtions are a block of statement that does a specific tasks.
+
+# def student(name1, name2): #keyword arguments we will write in this args
+#     print(name1, name2)
+
+# student(name1="Ayan", name2="Ahmad")
+# student(name2="Anjum", name1="Sarika")
+
+# def nameAge(name, age):
+#     print("Hi guys my name is ", name)
+#     print("Hi i am", age, "years old")
+
+# nameAge("rithik", 20)
+# nameAge("Ali", 21)
+# nameAge("ayan", 23)
+
+def f1():
+    a = "I am a new learner"
+    def f2():
+       
+        print(a)
+    f2()
+f1()
+
+def uni(x):
+    x[0] = 22
+
+list = [10, 20, 3, 234, 12]
+uni(list)
+print(list)
+
+
+def num(n):
+    return (n**2) + 5
+print(num(2))
+
+def fun2(*args, **kwargs):
+    
+
+
+
+
+
+
+
+
+
+ 
