ed091792dc46f7e853da39addcc8f9c557a8a193
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-17 23:00:13 +0530

    Add files via upload

--- lambda.py
+++ lambda.py
@@ -1,25 +1,24 @@
-m = float(input())      # Input mass
-v = float(input())      # Input velocity
+# mul = lambda x: x ** 2
+# print(mul(5))
 
-momentum = lambda m, v: m * v   # Lambda function to calculate momentum
-result = momentum (m, v), 2) 
-print(result)
+# add = lambda a, b, c: a + b + c
+# print(add(4, 5, 6))
 
-n = int(input())          # Take integer input
-r = lambda x: x * 5       # Define lambda function to multiply by 5
-print(r(n))               # Call lambda function and print result
+# x = int(input("x: "))
+# y = int(input("y: "))
+# z = int(input("z: "))
+# mm = lambda x, y, z: x**y + z
+# print(mm)
 
-words = ["apple", "banana", "cherry"]
-words.sort(key=lambda w: w[-1])
-print(words)  # Output: ['banana', 'apple', 'cherry']
+# check = lambda x: "even" if x % 2 == 0 else "odd"
+# print(check(8))
+# print(check(5))
+ 
 
-nums = [1, 2, 3, 4, 5]
-squared = list(map(lambda x: x**2, nums))
-print(squared)  # Output: [1, 4, 9, 16, 25]
+x = 10
+def glo(x):
+    x = 10
+    print(x)
 
-add_ten = lambda x: x + 10
-print(add_ten(5))   # Output: 15
-
-
-maximum = lambda a, b: a if a > b else b
-print(maximum(10, 15))  # Output: 15
+def local():
+    y = 20
