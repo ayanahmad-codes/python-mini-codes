b5cb6963ed6a6b20b988b739e16c37de9b8458d5
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-23 23:03:24 +0530

    Add files via upload

---
+++ ps2.py
@@ -0,0 +1,25 @@
+# def greet():
+#     print("Hello Ayan! How\'s going?")
+# greet()
+
+# #for loop
+
+# def nums():
+#     for i in range(1, 6):
+#         print(i)
+# nums()
+
+
+# def square(n):
+#     return n**2
+# print(n)
+
+
+n = int(input("enter a num: ")) 
+square = lambda n: n**2
+print(square(n))
+
+
+
+
+
