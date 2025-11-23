992bb4bd0e80a80ebc0b0275539987eaea878009
Ayan Ahmad <ayan.ahmad03600@gmail.com>
2025-11-23 23:02:59 +0530

    Add files via upload

---
+++ ps1-gpt.py
@@ -0,0 +1,63 @@
+# def name():
+#     print("Hello Ayan!")
+# name()
+
+
+# def nums():
+#     for i in range(1, 6):
+#         print(i)
+# nums()
+# global variable
+x = 10
+def glo(x):
+    globals
+
+    print("x: ",x)
+glo(x)
+
+
+def var():
+    a = 28
+    print("Local varible:",a)
+    global x
+    x = 40
+    print("global variable x:", x)
+var()
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(5):
+    print("hello boss")
+
+
+# str = input("enter words: ")
+# end=" " gives all data in horizontal line
+str = "python" 
+for ch in str:
+    print(ch, end=" ")
+
+
+for i in range(10, 0, -1):
+    print(i)
+
+
+for i in range(1, 50):
+    if i % 2 == 0:
+        print(i)
+
+# i = 1
+# while i <= 5:
+#     print(i, end=" ")
+#     i = i + 1
+
+# i = 1
+# while i <= 10:
+#     print(i*5, end=" ")
+#     i = i + 1
+
+i = 10
+while i >= 1:
+    print(i, end=" ")
+    i = i - 1
+
